3c79d4977e5dc970eb0f137155466c4058a15a34
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-02-05 07:08:48 +0900

    fix

--- pages/select/script.js
+++ pages/select/script.js
@@ -1,27 +1,30 @@
 var result = document.getElementById('result');
 var check =()=>{
-    num = parseFloat(result.r1_1.value)+parseFloat(result.r1_2.value)+parseFloat(result.r1_3.value);
-    console.log(num);
+    num1 = parseFloat(result.r1_1.value)+parseFloat(result.r1_2.value)+parseFloat(result.r1_3.value);
+    num2 = parseFloat(result.r2_1.value)+parseFloat(result.r2_2.value)+parseFloat(result.r2_3.value);
+    console.log(num1 + num2);
 };
 
 var list_length = document.getElementsByClassName('select-list').length;
 var badge_num = 0;
 
-var next =(e)=>{
-    if (badge_num > list_length) {badge_num = 0;}
+var next =()=>{
     list = document.getElementsByClassName('select-list')[badge_num];
-    list_class = list.getAttribute('class');
+    list_class = document.getElementsByClassName('select-list')[badge_num].getAttribute('class');
     if (list_class == 'select-list active') {
-        list.setAttribute('style', 'z-index: 1; opacity: 0%; transition: .5s;')
+        document.getElementsByClassName('select-list')[badge_num].setAttribute('class', 'select-list');
+        document.getElementsByClassName('select-list')[badge_num].setAttribute('style', 'z-index: 1; opacity: 0%; transition: .5s;');
         badge_num += 1;
-        list.setAttribute('style', 'z-index: 10; opacity: 100%; transition: .5s;')
-    }  
+        if (badge_num > (list_length-1)) {badge_num = 0;}
+        document.getElementsByClassName('select-list')[badge_num].setAttribute('class', 'select-list active');
+        document.getElementsByClassName('select-list')[badge_num].setAttribute('style', 'z-index: 10; opacity: 100%; transition: .5s;');
+    }
 };
 
 window.onload =()=> {
-    list_itm = document.getElementsByClassName('select-list');  
+    nextBtn = document.getElementsByClassName('select--next');  
     for(var i=0; i<list_length; i++){
-        list_itm[i].addEventListener('click', function(){
+        nextBtn[i].addEventListener('click', function(){
             next();
         });
     }
